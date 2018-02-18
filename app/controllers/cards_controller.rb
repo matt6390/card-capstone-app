7fa741c17901aafc9f0a9fd6da63d729b3d9d804
matt6390 <matthewstone639@gmail.com>
2018-02-18 17:03:11 -0600

    Done for the day, but making good progress with cards and users

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,8 +5,7 @@ class CardsController < ApplicationController
   end
 
   def show
-    card = Card.find_by(params[:id])
-
+    card = Card.find(params[:id])
     render json: card.as_json
   end
 
@@ -20,8 +19,30 @@ class CardsController < ApplicationController
                    )
     if card.save
      render json: card.as_json
-   else
-    render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    else
+      render json: {errors: card.errors.full_messages}, status: :unprocessable_entity
     end
+  end
+
+  def update
+    card = Card.find(params[:id])
+
+    card.name = params[:name] || card.name
+    card.description = params[:description] || card.description
+    card.element = params[:element] || card.element
+    card.race = params[:race] || card.race
+    card.rarity = params[:rarity] || card.rarity
+
+    if card.save
+     render json: card.as_json
+    else
+      render json: {errors: card.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    card = Card.find(params[:id])
+    card.destroy
+    render json: {message: "Card successfully destroyed"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   get "/cards" => 'cards#index'
   get "/cards/:id" => 'cards#show'
   post "/cards" => 'cards#create'
-  patch "/card/:id" => 'cards#update'
+  patch "/cards/:id" => 'cards#update'
+  delete "cards/:id" => 'cards#destroy'
+
+  post "/user_token" => 'user_token#create'
+  post "/users" => 'users#create'
+  delete "/users" => 'users#destroy'
 end
